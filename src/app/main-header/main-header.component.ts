8a4781ed3a5fa6ff3d1d14262ff285fd24d26140
Parminder Singh <psingh094@yahoo.com>
2018-11-08 18:16:18 -0800

    completed lab 3

---
+++ src/app/main-header/main-header.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-header',
+  templateUrl: './main-header.component.html',
+  styleUrls: ['./main-header.component.css']
+})
+export class MainHeaderComponent implements OnInit {
+
+  private firstName: string; 
+  private lastName: string;
+
+  constructor() {
+    this.firstName = 'Parminder';
+    this.lastName = 'Singh';
+  }
+  showGreeting() {
+    const banner = document.getElementById('banner');
+    banner.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName + '.';
+  }
+  hideGreeting() {
+    const banner = document.getElementById('banner');
+    banner.innerHTML = 'r1c2';
+  }
+
+  ngOnInit() {
+  }
+
+}
